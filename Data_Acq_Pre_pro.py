35480efdb18f2c2965e24ca16d1f741b872a33c8
Rishabh Arora <54496474+r1shabharora@users.noreply.github.com>
2022-11-09 23:22:05 +0000

    Update Data_Acq_Pre_pro.py

--- Data_Acq_Pre_pro.py
+++ Data_Acq_Pre_pro.py
@@ -5,10 +5,24 @@ Created on Wed Nov  9 22:55:12 2022
 @author: Rishabh Arora
 """
 #Importing necessary packages
+
+#Data manipulation realted imports
 import pandas as pdx
 import numpy as npx
-
 from datetime import datetime
 
-import winsound
+#Other main imports
+import requests
+from bs4 import BeautifulSoup
 import time;import json
+
+
+#Extra feature imports
+from random import randint
+import winsound
+
+#The URL from where data needs to be pulled
+rURL = 'https://www.google.com'
+
+#Capture response data coming from URL
+rdata = requests.get(rURL)
