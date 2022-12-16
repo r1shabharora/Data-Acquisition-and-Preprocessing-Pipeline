481e02dab8fcaaf218833c4b62c8b4ca49c362f1
Rishabh Arora <54496474+r1shabharora@users.noreply.github.com>
2022-12-16 19:31:33 +0000

    Finalization_1
    Created database connection using MongoDB
    Converted Data-Frame into JSON
    loaded JSON back to database

--- Data_Acq_Pre_pro.py
+++ Data_Acq_Pre_pro.py
@@ -4,20 +4,25 @@ Created on Wed Nov  9 22:55:12 2022
 
 @author: Rishabh Arora
 """
-#Importing necessary packages
+#Installing pymongo package
 !pip install "pymongo[srv]"
+
+#Importing necessary packages
 
 #Data manipulation realted imports
 import pandas as pdx
 import numpy as npx
 from datetime import datetime
 
+#Importing MongoDB packages
 import pymongo
 from pymongo import MongoClient
+
+#Importing OS package
 import os
 
+#Importing below package to parse the content securely
 from urllib.parse import quote_plus
-from pprint import pprint
 
 #Other main imports
 import requests
@@ -30,8 +35,10 @@ from random import randint
 import winsound
 
 try:
-    
+        
+    #Caputing start time of program
     s_time = datetime.now()
+    print('Program started on ',s_time)
     
     #Creating a blankk dataframe
     r_df = pdx.DataFrame()
@@ -50,8 +57,7 @@ try:
     
     #Printing the response status
     print("response.ok : {} , response.status_code : {}".format(r_response.ok , r_response.status_code))
-    
-    
+        
     #Checking if response is okay or not. In case the response is not 'OK' then program will auto-quit.
     if r_response.status_code == 200:
         print('Website respone ok')
@@ -65,9 +71,9 @@ try:
     print(r_response)
     
     #Parsing response data(content) into a variable
-    soup=BeautifulSoup(r_response.content,'lxml')
+    soup=BeautifulSoup(r_response.content,'lxml')  
     
-    
+    print('Starting loop')
     #Starting a loop so that each row of table data can be iterated. Each iteration will capture data for the new company
     for item in soup.select('.simpTblRow'):
         
@@ -79,7 +85,7 @@ try:
                   "CHANGE_P":item.select('[aria-label="% Change"]')[0].get_text(),
                   "M_CAP":item.select('[aria-label="Market Cap"]')[0].get_text(),
                   "AVERAGE VOLUME":item.select('[aria-label*="Avg Vol (3 month)"]')[0].get_text(),
-                  "PE_RATIO":item.select('[aria-label*="PE Ratio (TTM)"]')[0].get_text()}
+                  "PE_RATIO":item.select('[aria-label*="PE Ratio (TTM)"]')[0].get_text(),}
         
         #Data that was captured in dictionary is now being appended to the dataframe
         r_df = r_df.append(my_dict,ignore_index=True)
@@ -87,10 +93,9 @@ try:
     
     #Printing dataframe to see the captured or recorded data
     print(r_df)
-    
-    
+        
     #Database loading part
-    username = "Rishabh_connection"
+    username = quote_plus("Rishabh_connection")
     password = "Rish@9090#"
     host = "@cluster0.ov5lnlw.mongodb.net/?retryWrites=true&w=majority"
     
@@ -100,40 +105,38 @@ try:
     client = MongoClient(uri)
     server_api = ServerApi('1')
 
-    client = pymongo.MongoClient("mongodb+srv://Rishabh_connection:<password>@cluster0.ov5lnlw.mongodb.net/?retryWrites=true&w=majority", server_api=ServerApi('1'))
+    client = pymongo.MongoClient("mongodb+srv://"+quote_plus('Rishabh_connection')+":"+quote_plus('dJeFteltJwJ7q6pL')+"@cluster0.ov5lnlw.mongodb.net/?retryWrites=true&w=majority", server_api=ServerApi('1'))
     db = client.test
     print(db)
-
-        
-    client = pymongo.MongoClient("mongodb+srv://paul:E1IDRHJLFJbM5Q8z@cluster0.pudqg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-    client = pymongo.MongoClient("mongodb+srv://Rishabh_connection:Rish@9090#@cluster0.ov5lnlw.mongodb.net/?retryWrites=true&w=majority")
     
-    db = client["mydata"]
+    #Printing all databases in cluster
+    print(client.list_database_names())
+
+    #Specifying the name of the database as db
+    db = client["stock_db"]
+    #Specifying the name of the connection
     collection=db['listings']
     
+    #Assigning the collection of our database
+    collection=db.stock_collection
     
-    print(client.list_database_names())
+    #Printing all collections
     print(collection)
+    collection.drop()
+
+    #Printing the count of documents that are part of the collection
+    collection.count_documents({})
     
-    #import pypyodbc
-    #connection = pypyodbc.connect('Driver={ODBC Driver 17 for SQL Server};''Server=34.140.29.246;''Database=master;''encrypt=yes;''TrustServerCertificate=yes;''UID=sa;''PWD=Pr0gramming!',autocommit = True)
-    #connection = pypyodbc.connect('Driver={ODBC Driver 17 for SQL Server};''Server=34.140.29.246;''Database=Customer;''encrypt=yes;''TrustServerCertificate=yes;''UID=sa;''PWD=Pr0gramming!',autocommit = True)
+    #Putting dataframe in JSON so that it can later be used to update database
+    r_df.reset_index(inplace=True)
+    data_dict = r_df.to_dict("records")
+    collection.insert_many(data_dict)
+
+    #Re-printing the count of documents that are part of the collection
+    collection.count_documents({})
     
-    #cursor = connection.cursor()
-    #SQLCommand = ("CREATE TABLE stock(ID INT);")
-    #cursor.execute(SQLCommand)
-    #SQLCommand = ("INSERT INTO stock VALUES(?);")
-    #cursor.execute(SQLCommand,40)
-    #SQLCommand = ("INSERT INTO stock VALUES(my_dict);")
-    #cursor.execute(SQLCommand)
-    #SQLCommand = ("SELECT * FROM stock;")
-    #cursor.execute(SQLCommand)
-    #results = cursor.fetchone()
-    #print('First result is:',results[0])
-    #results = cursor.fetchone()
-    #print('Next result is:',results[0])
-    #print('done')
-    
+    print(collection.count_documents({}),'Records have been saved in database')
+        
     #Notify user by playing 3 beep sounds
     freq = 5000
     duration = 500
@@ -145,7 +148,7 @@ try:
         time.sleep(0.2)
     
     print('Time taken :',str(datetime.now()-s_time).split('.')[0])
-
+    
 except:
     print('Unknown error in program')
 
